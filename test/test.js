5e8e0b435f86d2c8d2f346965f94929ed85b2b6d
Jianfeng <ginfungchan@gmail.com>
2016-10-16 17:24:55 -0400

    --wip--

--- test/test.js
+++ test/test.js
@@ -152,5 +152,23 @@ describe('suncalc', function() {
     });
   });
 
+  describe('2015-06-23 in extreme latitude', function() {
+    var solarCalc;
+
+    beforeEach(function() {
+      solarCalc = new SolarCalc(
+        new Date('Jun 23 2015'),
+        82.4508,
+        -62.5056,
+        -4,
+        false
+      );
+    });
+
+    it('test leap year', function(){
+      assert.equal(true, solarCalc.sun.isLeapYear(2000));
+    });
+  });
+
 });
 
