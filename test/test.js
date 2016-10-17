00aa58b8b0aa841bc89a2fb0e4135c2a2386820d
Jianfeng <ginfungchan@gmail.com>
2016-10-17 14:34:43 -0400

    fuzz t1

--- test/test.js
+++ test/test.js
@@ -152,23 +152,5 @@ describe('suncalc', function() {
     });
   });
 
-  describe('2015-06-23 in extreme latitude', function() {
-    var solarCalc;
-
-    beforeEach(function() {
-      solarCalc = new SolarCalc(
-        new Date('Jun 23 2000'),
-        82.4508,
-        -62.5056,
-        -4,
-        false
-      );
-    });
-
-    it('test leap year', function(){
-      assert.equal(956707200000, solarCalc.goldenHourEnd.getTime());
-    });
-  });
-
 });
 
